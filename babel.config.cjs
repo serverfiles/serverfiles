5b0e3adc122e1f2fd57cb43c042770a3e8cfb055
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-07-18 09:38:22 +0530

    🛠 uses newer ESBuild to bundle all dependencies to CommonJS

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,13 +1,3 @@
 module.exports = {
     plugins: [require('@babel/plugin-syntax-top-level-await')],
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    esmodules: true,
-                },
-            },
-        ],
-    ],
 }
