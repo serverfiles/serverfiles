ec0b5b7debf90e6808ec4ded8574339a95b365b8
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-07-26 10:03:30 +0530

    ⚡ properties in config files are now validated

--- src/cmds/config/write/02-parse.js
+++ src/cmds/config/write/02-parse.js
@@ -3,9 +3,22 @@
  *  Created On 25 July 2021
  */
 
+import { promise } from '@vasanthdeveloper/utilities'
 import fs from 'fs/promises'
 import grayMatter from 'gray-matter'
 import hbs from 'handlebars'
+import Joi from 'joi'
+import path from 'path'
+
+const modSchema = Joi.object({
+    name: Joi.string(),
+    path: Joi.string().required(),
+    content: Joi.string().required(),
+    executables: Joi.array().items(Joi.string()),
+    file: Joi.string().required(),
+    hook: Joi.string(),
+    sudo: Joi.bool().default(false),
+})
 
 export default async ({ data, files }) => {
     const returnable = []
@@ -21,15 +34,34 @@ export default async ({ data, files }) => {
         // parse front-matter
         const { content, data: props } = grayMatter(text)
 
-        // push a new module definition into our returnable
-        returnable.push({
+        const mod = {
             ...props,
             ...{
                 content: content.trim() + '\n',
                 file: file.file,
                 hook: file.hook,
             },
-        })
+        }
+
+        // validate the module definition for any potentially
+        // invalid properties
+        const { error, returned } = await promise.handle(
+            modSchema.validateAsync(mod),
+        )
+
+        if (error) {
+            // code 4: invalid properties for a config file
+            console.log(
+                `Invalid properties ${error.message} in 👇\n${path.relative(
+                    process.cwd(),
+                    file.file,
+                )}`,
+            )
+            process.exit(4)
+        }
+
+        // push a new module definition into our returnable
+        returnable.push(returned)
     }
 
     return returnable
