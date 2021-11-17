60884fbffa0b8634d01fde25ca5d549d78fbb666
Vasanth Developer <vsnthdev@gmail.com>
2021-11-17 22:01:07 +0530

    ✨ | created the help message

---
+++ help.bash
@@ -0,0 +1,21 @@
+#
+#  Registers the help message as a function.
+#  Created On 17 November 2021
+#
+
+display_help() {
+    echo ""
+    echo " ${WHITE}${BOLD}USAGE${RESET}"
+    echo "    ${WHITE}$(basename $0)${RESET} [${AMBER}options${RESET}] <${BLUE}config script${RESET}>"
+    echo ""
+    echo "    Dynamically write ⚗️ config files to 🐧 Linux servers."
+    echo ""
+    echo " ${WHITE}${BOLD}OPTIONS${RESET}"
+    echo "    ${AMBER}-v${RESET}, ${AMBER}--verbose${RESET}    Show ${DIM}before_write()${RESET} & ${DIM}after_write()${RESET} outputs"
+    echo "    ${AMBER}-h${RESET}, ${AMBER}--help${RESET}       Show this help message"
+    echo ""
+    echo "    Licensed under ${WHITE}${BOLD}MIT${RESET}"
+    echo "    Read more ${WHITE}${BOLD}${UNDERLINE}https://github.com/serverfiles/serverfiles${RESET}"
+    echo ""
+    echo "    See \"${WHITE}${BOLD}$(basename $0)${RESET} ${AMBER}--help${RESET}\""
+}
---
+++ serverfiles.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+#  Updates a config file by taking a serverfiles config script.
+#  Created On 17 November 2021
+#
+
+source ./variables.bash
+source ./help.bash
+
+display_help
