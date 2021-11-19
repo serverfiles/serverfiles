baeac15f22efacb196e1a21d24cf16501ff1be04
Vasanth Developer <vsnthdev@gmail.com>
2021-11-19 20:08:36 +0530

    ✨ | only allows running as root

--- serverfiles.bash
+++ serverfiles.bash
@@ -17,6 +17,7 @@ AMBER=$(tput setaf 11)
 BLUE=$(tput setaf 14)
 MAGENTA=$(tput setaf 5)
 GRAY=$(tput setaf 0)
+RED=$(tput setaf 9)
 
 # automatically link itself as a universal execiable
 source "$SCRIPTPATH/install.bash"
@@ -26,8 +27,21 @@ source "$SCRIPTPATH/args/defaults.bash"
 source "$SCRIPTPATH/args/help.bash"
 source "$SCRIPTPATH/args/args.bash"
 
+# only allow running as root, so we can
+# set permissions & ownership correctly
+if [[ $(id -u) -ne 0 ]]; then
+    echo "🚨 ${RED}Error:${RESET} Please run this script with ${WHITE}${BOLD}sudo${RESET}, so permissions &"
+    echo "   ownership can be set correctly"
+    exit 1
+fi
+
 # register the install_serverfile() function
 source "$SCRIPTPATH/serverfile.bash"
+
+# handle when no config scripts are passed
+if [ -z $@ ]; then
+    display_help
+fi
 
 # loop through each config script provided
 for CSCRIPT in $@; do
