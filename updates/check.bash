60d65a5024ac55782afe59668a9804c39d7aba00
Vasanth Developer <vsnthdev@gmail.com>
2021-11-19 23:17:46 +0530

    🐛 | fixed .update-available file gets created in wrong folder

--- updates/check.bash
+++ updates/check.bash
@@ -9,7 +9,7 @@
 # next time when the main script is launched to pull the updates
 
 SCRIPT=$(realpath $0)
-SCRIPTPATH=$(dirname $SCRIPT)
+SCRIPTPATH=$(dirname $(dirname $SCRIPT))
 
 # grab the latest commit SHA from GitHub main branch
 LATEST_SHA=$(curl -sS https://api.github.com/repos/serverfiles/serverfiles/commits?per_page=1 | grep "sha" | head -n 1 | awk '$1=$1')
@@ -22,5 +22,5 @@ CURRENT_SHA=$(git rev-parse HEAD)
 # we assume we've got an update if both SHAs are
 # not equal
 if [[ $CURRENT_SHA != $LATEST_SHA ]]; then
-    touch .update-available
+    touch "$SCRIPTPATH/.update-available"
 fi
